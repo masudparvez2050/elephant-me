e585eaba315608f13e5764880953ae812330e009
MASUDUR RAHMAN <masudparvez2050@gmail.com>
2024-05-12 18:42:11 +0600

    12-05-24  Update

--- src/pages/section/Gallary.jsx
+++ src/pages/section/Gallary.jsx
@@ -1,7 +1,11 @@
 const Gallary = () => {
   return (
-    <div className="relative w-full h-[100vh]  bg-gray-100 text-white grid place-items-center z-10">
-      Gallary
+    <div className="relative w-full h-[100vh]  bg-gray-100 text-white grid place-items-center z-10 overflow-hidden">
+      <img
+        className="absolute w-[120vw]    opacity-[0.2]"
+        alt=""
+        src="/public/back_gallary.png"
+      />
     </div>
   );
 };
